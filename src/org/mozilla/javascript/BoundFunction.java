68dfd13ce46709c2e4c794cd3709376f6fe49714
Matthew Olsson <matthewcolsson@gmail.com>
2019-12-06 18:44:21 -0600

    Bound function names

--- src/org/mozilla/javascript/BoundFunction.java
+++ src/org/mozilla/javascript/BoundFunction.java
@@ -94,6 +94,15 @@ public class BoundFunction extends BaseFunction {
         return length;
     }
 
+    @Override
+    public String getFunctionName() {
+        if (targetFunction instanceof BaseFunction) {
+            return "bound " + ((BaseFunction) targetFunction).getFunctionName();
+        }
+
+        return "";
+    }
+
     private Object[] concat(Object[] first, Object[] second) {
         Object[] args = new Object[first.length + second.length];
         System.arraycopy(first, 0, args, 0, first.length);
